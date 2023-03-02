d2d3021659c0a8a6ad5d812f00c7f9e7dddbf448
Milla22 <marti.ludmilla@orange.fr>
2023-03-02 13:42:27 +0100

    1 div slide, 1 tabeau index, suppression des console.log, ne slide plus sur X, change img et tagline (changeSlide au click

--- script.js
+++ script.js
@@ -1,65 +1,64 @@
-const img = document.querySelectorAll(".slider-img");
-img[0].src = "./images/slide1.jpg";
-img[1].src = "./images/slide2.jpg";
-img[2].src = "./images/slide3.jpg";
-img[3].src = "./images/slide4.png";
 
-const txtSlides = document.querySelectorAll('p');
-txtSlides[0].innerHTML = 'Impressions tous formats <span>en boutique et en ligne</span>';
-txtSlides[1].innerHTML = 'Tirages haute définition grand format <span>pour vos bureaux et events</span>';
-txtSlides[2].innerHTML = 'Grand choix de couleurs <span>de CMJN aux pantones</span>';
-txtSlides[3].innerHTML = 'Autocollants <span>avec découpe laser sur mesure</span>';
+const slide = document.querySelectorAll(".slider-img"); const tagLine = document.querySelectorAll('p');
+const slides = document.querySelectorAll('.slider')[
+
+slide[0].src = "./images/slide1.jpg",
+tagLine[0].innerHTML = 'Impressions tous formats <span>en boutique et en ligne</span>',
+
+slide[1].src = "./images/slide2.jpg",
+tagLine[1].innerHTML = 'Tirages haute définition grand format <span>pour vos bureaux et events</span>',
+
+slide[2].src = "./images/slide3.jpg",
+tagLine[2].innerHTML = 'Grand choix de couleurs <span>de CMJN aux pantones</span>',
+
+slide[3].src = "./images/slide4.png",
+tagLine[3].innerHTML = 'Autocollants <span>avec découpe laser sur mesure</span>'
+];
 
 function Slider() {
-    const carouselSlides = document.querySelectorAll('.slide');
-    const btnPrev = document.querySelector('.prev');
-    btnPrev.src = "./images/arrow_left.png";
-    const btnNext = document.querySelector('.next');
-    btnNext.src = "./images/arrow_right.png";
-    const dotsSlide = document.querySelector('.dots-container');
-    let currentSlide = 0;
+const btnPrev = document.querySelector('.prev');
+const btnNext = document.querySelector('.next');
+const dotsSlide = document.querySelector('.dots-container');
+let currentSlide = 0;
 
-    const activeDot = function (slide) {
-        document.querySelectorAll('.dot').forEach(dot => dot.classList.remove('active'));
-        document.querySelector(`.dot[data-slide="${slide}"]`).classList.add('active');
-    };
-    activeDot(currentSlide);
+const activeDot = function (slider) { 
+    document.querySelectorAll('.dot').forEach (dot =>dot.classList.remove('active')); 
+    document.querySelector(`.dot[data-slide="${slider}"]`).classList.add('active');
+};
+activeDot(currentSlide);
 
-    const changeSlide = function (slides) {
-        carouselSlides.forEach((slide, index) => (slide.style.transform = `translateX(${100 * (index - slides)}%)`));
-    };
-    changeSlide(currentSlide);
+const changeSlide = function (sliders) {
+    slide.forEach((slider, index)=> (slider.style.transform = `translate(${100 *(index - sliders)}%)`));
+    tagLine.forEach((slider, index)=> (slider.style.transform = `translate(${100 * (index - sliders)}%)`));
+};
+changeSlide(currentSlide);
 
-    btnNext.addEventListener('click', function () {
-        currentSlide++; 
-        if (carouselSlides.length -1 < currentSlide) {
-            currentSlide = 0;
-        };
-        changeSlide(currentSlide)
-        activeDot(currentSlide);
-    });
-    console.log[btnNext];
+btnNext.addEventListener('click', function () { 
+    currentSlide++;
+    if (slide.length -1 < currentSlide) {
+    currentSlide = 0; 
+};
+changeSlide(currentSlide) 
+activeDot(currentSlide);
+});
 
-    btnPrev.addEventListener('click', function () {
-        currentSlide--;
-        if (0 > currentSlide) {
-            currentSlide = 3;
-        }; 
-        changeSlide(currentSlide);
-        activeDot(currentSlide);
-    });
-    console.log[btnPrev]
-    
-    dotsSlide.addEventListener('click', function (e) {
-        if (e.target.classList.contains('dot')) {
-            const slide = e.target.dataset.slide;
-            changeSlide(slide);
-            activeDot(slide);
-        }
-    });
-    console.log[dotsSlide]
-    };
-Slider();
+btnPrev.addEventListener('click', function () { 
+    currentSlide--;
+    if (0 > currentSlide) {
+    currentSlide = 3; 
+};
+changeSlide(currentSlide);
+activeDot(currentSlide); 
+});
+
+dotsSlide.addEventListener('click', function (e) { 
+    if (e.target.classList.contains('dot')) {
+    const slide = e.target.dataset.slide;
+changeSlide(slide);
+activeDot(slide); 
+}
+});
+}; Slider();
 
 
 
